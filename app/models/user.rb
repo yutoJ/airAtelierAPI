6be9932d33a97c18451f4438fdaa5f79ecd653eb
yutoJ <yutoogata4@gmail.com>
2017-09-24 18:20:58 +0900

    api pj

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :confirmable
+
+  validates :fullname, presence: true, length: {maximum: 50}
+
+  has_many :rooms
+  has_many :reservations
+
+  has_many :guest_reviews, class_name: "GuestReview", foreign_key: "guest_id"
+  has_many :host_reviews, class_name: "HostReview", foreign_key: "host_id"
+  has_many :notifications
+
+  has_one :setting
+  after_create :add_setting
+
+  def add_setting
+    Setting.create(user: self, enable_sms: true, enable_email: true)
+  end
+
+  def self.from_omniauth(auth)
+    user = User.where(email: auth.info.email).first
+    if user
+      return user
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.fullname = auth.info.name
+        user.image = auth.info.image
+        user.uid = auth.uid
+        user.provider = auth.provider
+
+        # user.skip_confirmation!
+      end
+    end
+  end
+
+end
